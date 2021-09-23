fd87ec1cc12e78f36dd8fa9dda90cbccf58a40a3
Ashish Nallana <ashishnallana@gmail.com>
2021-09-23 16:49:20 +0530

    Create App2.js

---
+++ App2.js
@@ -0,0 +1,53 @@
+import './App.css';
+import { useState, useEffect } from 'react';
+import firebase from './Firebase';
+
+function App() {
+
+  const ref = firebase.firestore().collection("developers")
+  // console.log(ref);
+
+  const [data, setdata] = useState([])
+  const [loader, setloader] = useState(true)
+
+  // function getData(){
+ // *An initial call using the callback you provide creates a document snapshot immediately with the current contents 
+ // *of the single document. Then, each time the contents change, another call updates the document snapshot.
+  //   ref.onSnapshot((querySnapshot) => {
+  //     const items = []
+  //     querySnapshot.forEach((doc) => {
+  //       items.push(doc.data())
+  //     })
+  //     setdata(items)
+  //     setloader(false)
+  //   })
+  // }
+
+  function getData2(){
+    ref.get().then((item) => {
+      const items = item.docs.map((doc) => doc.data())
+      setdata(items)
+      setloader(false)
+    })
+  }
+
+  useEffect(() => {
+    // getData()
+    getData2()
+  }, [])
+
+  return (
+    <div className="App">
+      <h1>#firebase firestore database</h1>
+      
+      {loader === false && (data.map((dev) => (
+        <div key={dev.id}>
+          <h1>Name : {dev.name}</h1>
+          <p>Skill : {dev.skill}</p>
+        </div>
+        )))}
+    </div>
+  );
+}
+
+export default App;
